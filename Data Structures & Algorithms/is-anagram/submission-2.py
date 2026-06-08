a10a59fd8871f33192fd4e4eb953c576dcbf4ca6
IrhaNaveed <50791452+IrhaNaveed@users.noreply.github.com>
2026-06-08 15:07:20 +0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        dict_s = {}
+        for value in s:
+            if value in dict_s:
+                dict_s[value] += 1
+            else:
+                dict_s[value] = 1
+        
+        for val in t:
+            if val in dict_s and dict_s[val] != 0:
+                dict_s[val] -=1
+            else:
+                return False
+        
+        #check if the dict_s contains all the 0 if not then it's not anagram
+        for val in dict_s.values():
+            if val != 0:
+                return False
+        return True
